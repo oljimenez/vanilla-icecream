fabdef1a87a7b23c7ab38141d310a3fb23a62e7e
oscarprogrammer1996 <75763255+oscarprogrammer1996@users.noreply.github.com>
2022-08-20 09:45:38 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,13 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { Box } from "components/box";
 import Link from "next/link";
 import { Flex } from "components/flex";
 
-const Home: NextPage = () => {
-  const router = useRouter();
-
-  async function goToVanillaExtract() {
-    await router.push("/vanilla");
-  }
-
-  async function goToMagnify() {
-    await router.push("/magnify");
-  }
-
-  async function goToInternalization() {
-    await router.push("/internalization");
-  }
-
-  return (
-    <Flex itemSize={"full"} placeItems={"center"} gap={"lg"}>
-      <Link href={{ pathname: "/vanilla" }}>
-        <button>Vanilla Extract</button>
-      </Link>
-      <Link href={{ pathname: "/supabase" }}>
-        <button>Supabase</button>
-      </Link>
-    </Flex>
-  );
-};
+const Home: NextPage = () => (
+  <Flex itemSize={"full"} placeItems={"center"} gap={"lg"}>
+    <Link href={{ pathname: "/vanilla" }}>
+      <button>Vanilla Extract</button>
+    </Link>
+  </Flex>
+);
 
 export default Home;
