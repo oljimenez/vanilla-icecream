a30fc61eec69850fdc6a43bcf6af0649a8c46e55
oscarprogrammer1996 <75763255+oscarprogrammer1996@users.noreply.github.com>
2022-08-25 16:02:11 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 import { Button } from 'components/interaction';
 import { HStack, VStack } from 'components/layout';
 import { Text } from 'components/text';
-import { Box } from 'components/layout/box';
 
 const Home: NextPage = () => {
     //here your typescript
@@ -11,9 +10,6 @@ const Home: NextPage = () => {
         <VStack flexPlaceItems={'center'} itemSize={'fullViewport'} gap={'lg'}>
             <h1>Build with Vanilla Kit</h1>
             <VStack flexPlaceItems={'center'}>
-                <Box display={'flex'} justifyContent={'center'} padding={'sm'}>
-                    Example
-                </Box>
                 <Text>Flexbox</Text>
                 <HStack flexPlaceItems={'center'} gap={'lg'}>
                     <Button variant={{ style: 'primary' }}>Primary</Button>
